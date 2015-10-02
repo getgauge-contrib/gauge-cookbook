12009d9219e8b998c57fd31245f725d3e96de044
Ketan Padegaonkar <KetanPadegaonkar@gmail.com>
2015-10-02 22:29:41 +0530

    Fix foodcritic warnings

--- metadata.rb
+++ metadata.rb
@@ -4,9 +4,11 @@ maintainer_email 'ketanpadegaonkar@gmail.com'
 license          'MIT'
 description      'Installs/Configures gauge'
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.2.0'
 
 
 %w{redhat centos scientific fedora amazon oracle debian ubuntu suse opensuse}.each do |os|
   supports os
 end
+
+suggests 'chocolatey'
