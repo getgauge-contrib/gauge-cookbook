3ecdf3299f562633eea302c72317a18360f2ba16
Ketan Padegaonkar <KetanPadegaonkar@gmail.com>
2015-10-09 23:36:18 +0530

    Make plugin provider idempotent

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,6 +3,10 @@ Gauge CHANGELOG
 
 This file is used to list changes made in each version of the gauge cookbook.
 
+v0.3.0 (2015-10-09)
+-------------------
+* make the plugin provider idempotent
+
 v0.2.0 (2015-10-02)
 -------------------
 * fix some foodcritic warnings
--- providers/plugin.rb
+++ providers/plugin.rb
@@ -3,6 +3,7 @@ action :install do
     user  new_resource.user
     group new_resource.group
     environment ({ 'HOME' => node['etc']['passwd'][new_resource.user]['dir'], 'USER' => new_resource.user })
+    creates ::File.join(node['etc']['passwd'][new_resource.user]['dir'], '.gauge', 'plugins', new_resource.name)
   end
 end
 
