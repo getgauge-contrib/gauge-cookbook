ee4c331c639c647dd41e6a2e7a770ffcf0c4868f
Ketan Padegaonkar <KetanPadegaonkar@gmail.com>
2015-10-10 21:54:11 +0530

    Fix windows install

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,6 +3,10 @@ Gauge CHANGELOG
 
 This file is used to list changes made in each version of the gauge cookbook.
 
+v0.4.0 (2015-10-09)
+-------------------
+* fix windows installation
+
 v0.3.0 (2015-10-09)
 -------------------
 * make the plugin provider idempotent
--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'ketanpadegaonkar@gmail.com'
 license          'MIT'
 description      'Installs/Configures gauge'
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.3.0'
+version          '0.4.0'
 
 
 %w{redhat centos scientific fedora amazon oracle debian ubuntu suse opensuse}.each do |os|
