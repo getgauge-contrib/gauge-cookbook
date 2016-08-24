a66e784feac28eeecaccc6ca6185dd39575a5fb8
Ketan Padegaonkar <ketanpadegaonkar@gmail.com>
2016-08-24 10:35:42 +0530

    Bump to 0.6.1

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,20 +1,20 @@
-default['gauge']['version']  = "0.5.0"
+default['gauge']['version']  = "0.6.1"
 
 if platform_family?('windows')
   if node['kernel']['machine'] =~ /x86_64/
     default['gauge']['url']      = "https://github.com/getgauge/gauge/releases/download/v#{node['gauge']['version']}/gauge-#{node['gauge']['version']}-windows.x86_64.exe"
-    default['gauge']['checksum'] = 'e58e7752a1d849a82d518dc1230e11d9a98f8d2b0d56fd6a2ae1ec315d63f0bc'
+    default['gauge']['checksum'] = 'a80d25cf0b77000a883ee4e62cc6c27faea407930766f65f1437cf04671b970f'
   else
     default['gauge']['url']      = "https://github.com/getgauge/gauge/releases/download/v#{node['gauge']['version']}/gauge-#{node['gauge']['version']}-windows.x86.exe"
-    default['gauge']['checksum'] = '2247f384461d25f5e0c9f5c59f124cc05317c617e93e2d44ece84eb2440e02d7'
+    default['gauge']['checksum'] = '8a422cff743ef4c55e6aa74f18fa3c897a55871446a7cdf6cd7fbfda554f1135'
   end
 else
   if node['kernel']['machine'] =~ /x86_64/
     default['gauge']['url']      = "https://github.com/getgauge/gauge/releases/download/v#{node['gauge']['version']}/gauge-#{node['gauge']['version']}-linux.x86_64.zip"
-    default['gauge']['checksum'] = '0ccc45cf9fe614126fef3847ffc13744f2673e653782b613f3ced9f2da67bc5d'
+    default['gauge']['checksum'] = '727e75d5b09dbd79a82e9ec3cbff1b81983d0b5940fc8017a0e664c402ac38ac'
   else
     default['gauge']['url']      = "https://github.com/getgauge/gauge/releases/download/v#{node['gauge']['version']}/gauge-#{node['gauge']['version']}-linux.x86.zip"
-    default['gauge']['checksum'] = '201d181bd8e583d841c909c285fa45876339bfb970c3c0ad56597ac760704bcf'
+    default['gauge']['checksum'] = 'e0b041f0c9e0bff08b147107e4f2229ead2aeda1908008ac5a1268895b04a0ac'
   end
 end
 
